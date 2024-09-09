be2fde44acce95a6d3506058f7b3b40b1365b140
sara <estilaeisara@gmail.com>
2024-09-09 21:56:59 +0330

    Create today's tasks and change profile

--- src/main.js
+++ src/main.js
@@ -1,6 +1,18 @@
+const create = document.querySelector(".createTask");
+const newTask = document.querySelector(".newTask");
+const closeTask = document.querySelector(".closeTask");
+const taskForm = document.querySelector(".taskForm");
+const priority = document.querySelector(".priority");
+const priorityButton = document.querySelector(".priorityButton");
+const setPriority = document.querySelector(".setPriority");
+const profileImageButton = document.getElementById("profileImageButton");
+const profileImageInput = document.getElementById("profileImageInput");
+const profileImage = document.getElementById("profileImage");
+
 document.addEventListener("DOMContentLoaded", () => {
   const today = new Date();
   const jalaliDate = jalaali.toJalaali(today);
+
   const weekDays = [
     "یک‌شنبه",
     "دوشنبه",
@@ -11,6 +23,7 @@ document.addEventListener("DOMContentLoaded", () => {
     "شنبه",
   ];
   const dayOfWeek = weekDays[(today.getDay() + 6) % 7];
+
   const persianMonths = [
     "فروردین",
     "اردیبهشت",
@@ -26,9 +39,44 @@ document.addEventListener("DOMContentLoaded", () => {
     "اسفند",
   ];
   const month = persianMonths[jalaliDate.jm - 1];
+
   const day = jalaliDate.jd;
   const year = jalaliDate.jy;
   const persianDate = `امروز، ${dayOfWeek}، ${day} ${month} ${year}`;
 
   document.querySelector(".date").textContent = persianDate;
+});
+
+profileImageButton.addEventListener("click", () => {
+  profileImageInput.click();
+});
+
+profileImageInput.addEventListener("change", (event) => {
+  const file = event.target.files[0];
+
+  if (file && file.type.startsWith("image/")) {
+    const readerimage = new FileReader();
+
+    readerimage.onload = (e) => {
+      profileImage.src = e.target.result;
+    };
+
+    readerimage.readAsDataURL(file);
+  }
+});
+
+create.addEventListener("click", () => {
+  newTask.classList.remove("hidden");
+});
+closeTask.addEventListener("click", () => {
+  newTask.classList.add("hidden");
+  taskForm.reset();
+});
+priorityButton.addEventListener("click", () => {
+  if (setPriority.classList.contains("hidden")) {
+    priority.src = "./assets/Frame 1000005506.png";
+  } else {
+    priority.src = "./assets/Frame 1000005505.png";
+  }
+  setPriority.classList.toggle("hidden");
 });
